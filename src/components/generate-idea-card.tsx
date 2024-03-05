9b66e572bd0364bddabbc777e149e077fec2e7ff
dmarts05 <dmarts05@estudiantes.unileon.es>
2024-03-05 22:14:35 +0100

    feat: improve leading

--- src/components/generate-idea-card.tsx
+++ src/components/generate-idea-card.tsx
@@ -9,13 +9,13 @@ export function GenerateIdeaCard({ idea }: GenerateIdeaCardProps) {
     <div className="flex items-center justify-center w-full max-w-5xl overflow-hidden h-96">
       <MaskContainer
         revealText={
-          <p className="font-bold leading-loose text-center text-7xl text-slate-800">
+          <p className="font-bold leading-normal text-center text-7xl text-slate-800">
             ¡Revela el secreto!
           </p>
         }
         className="w-full h-full border rounded-md"
       >
-        <span className="text-7xl leading-loose text-[#3B82F6]">{idea}</span>
+        <span className="text-7xl leading-normal text-[#3B82F6]">{idea}</span>
       </MaskContainer>
     </div>
   );
