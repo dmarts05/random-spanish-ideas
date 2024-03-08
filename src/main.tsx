9a5273c6b64b2ad5b5e66cd0c1acc7dfbe761468
dmarts05 <dmarts05@estudiantes.unileon.es>
2024-03-08 18:56:28 +0100

    fix: fix 404 when refreshing

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HomePage } from "./pages/home-page";
 import { GenerateIdeaPage } from "./pages/generate-idea-page";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 // Enable dark mode
 window.document.documentElement.classList.add("dark");
 
 // Create the router
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/random-spanish-ideas/",
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: "/random-spanish-ideas/generate-idea",
+    path: "/generate-idea",
     element: <GenerateIdeaPage />,
   },
-]);
+]
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
